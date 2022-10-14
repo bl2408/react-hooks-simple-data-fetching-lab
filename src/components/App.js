a04eda5d403f0e2e172127b3501e1ec1abe11e31
Brian Lambert <brianlambert2408@gmail.com>
2022-10-14 13:15:11 +1100

    Completed tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+function App(){
+
+    const [dogImg, setDogImg] = useState("");
+    
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>{
+           // console.log(data)
+            if(data.status==="success"){
+                setDogImg(data.message);
+            }else{
+                //failed request logic here
+            }
+        })
+    },[]);
+
+     return (
+        <>
+            {dogImg ? <img src={dogImg} alt="A Random Dog" /> : <p>Loading...</p>}
+        </>
+     );
+}
+
+export default App;
